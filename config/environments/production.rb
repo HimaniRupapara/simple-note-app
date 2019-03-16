3365320711bf1105e2144ec8a9f4c06a4d6802a3
HimaniRupapara <hru@narola.email>
2019-03-16 11:14:14 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'quiet-plains-55573.herokuapp.com/', protocol: 'http' }
+  # config.action_mailer.default_url_options = { host: 'quiet-plains-55573.herokuapp.com/', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
